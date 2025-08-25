db932b10721a3fbcc046be2d78e51ad513c9bc90
lej9310 <lej9310@gmail.com>
2025-08-25 16:27:31 +0900

    8/25

--- proj-list/vite.config.js
+++ proj-list/vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  base: mode === 'production' ? '/vue-site/' : '/',
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [vue()],
+    base: mode === 'production' ? '/vue-site/' : '/',
+  }
 })
