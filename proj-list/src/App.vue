c0e1a0a0e243b28bf54424fe4e88a593dbb1fdd0
lej9310 <lej9310@gmail.com>
2025-08-25 14:22:35 +0900

    8/25

---
+++ proj-list/src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref } from 'vue';
+import Ex1 from './components/day2_0825/w3-proj/src/components/D1_bind.vue';
+import Ex2 from './components/day2_0825/w3-proj/src/components/D2_if.vue';
+import Ex3 from './components/day2_0825/w3-proj/src/components/D3_show.vue';
+import Ex4 from './components/day2_0825/w3-proj/src/components/D4_for.vue';
+
+const pages = { Ex1, Ex2, Ex3, Ex4 }
+const activeComp = ref("Ex1")
+</script>
+
+<template>
+  <!-- day2_0825 -->
+  <!-- <Ex1 />
+  <Ex2 />
+  <Ex3 />
+  <Ex4 /> -->
+  <button @click="activeComp = 'Ex1'">v-bind</button>
+  <button @click="activeComp = 'Ex2'">v-if</button>
+  <button @click="activeComp = 'Ex3'">v-show</button>
+  <button @click="activeComp = 'Ex4'">v-for</button>
+
+  <div id="comp_view">
+    <KeepAlive>
+      <component :is="pages[activeComp]"></component>
+    </KeepAlive>
+  </div>
+</template>
+
+<style scoped>
+button {
+  margin: 5px;
+  filter: drop-shadow(0 0 1.5px grey);
+}
+
+#comp_view {
+  margin-top: 20px;
+  border: 1px solid black;
+  border-radius: 10px;
+}
+</style>
