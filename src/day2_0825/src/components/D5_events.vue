c8e391bc7ec45c1b288bf44b6bfebfeb1056ecbe
lej9310 <lej9310@gmail.com>
2025-08-25 17:35:59 +0900

    8/25

--- src/day2_0825/src/components/D5_events.vue
+++ src/day2_0825/src/components/D5_events.vue
@@ -1,14 +1,34 @@
 <!-- vbase-3-setup -->
 <template>
     <div>
+        <img src="../assets/img_moose.jpg" alt="">
+        <p>Moose count: {{ count }}</p>        
+        <button type="button" @click="count++">Count moose</button>
 
     </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 
+const count = ref(0)
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+div>img {
+    width: 90%;
+    height: auto;
+}
 
+div {
+    border: dashed black 1px;
+    width: 100%;
+    padding: 20px;
+}
+
+button {
+    border: solid black 1px;
+    border-radius: 10ch;
+    box-shadow: 1px;
+}
 </style>
